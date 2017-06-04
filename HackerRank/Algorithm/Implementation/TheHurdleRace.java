e05407917f077d94646ddfa48895416dbe592f0c
Piyush Dusad <dusadpiyush96@gmail.com>
2017-06-04 23:42:39 +0530

    add TheHurdleRace.java

---
+++ HackerRank/Algorithm/Implementation/TheHurdleRace.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] height = new int[n];
+        for(int height_i=0; height_i < n; height_i++){
+            height[height_i] = in.nextInt();
+        }
+        // your code goes here
+        Arrays.sort(height);
+        if(k>=height[n-1]){
+            System.out.println(0);
+        }
+        else{
+            System.out.println(Math.abs(k-height[n-1]));
+        }
+    }
+}
