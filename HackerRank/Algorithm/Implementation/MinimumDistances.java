a8147b45a6de6acab0e90a40b408277ab47ead2e
Piyush Dusad <dusadpiyush96@gmail.com>
2018-05-28 09:02:43 +0530

    add MinimumDistances.java

---
+++ HackerRank/Algorithm/Implementation/MinimumDistances.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumDistances function below.
+    static int minimumDistances(int[] a) {
+        ArrayList<Integer> myList = new ArrayList<Integer>();
+        for(int i=0;i<a.length-1;i++){
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]==a[j]){
+                    myList.add(Math.abs(i-j));
+                }
+            }
+        }
+        if(myList.size()==0) return -1;
+        Collections.sort(myList);
+        return myList.get(0);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int result = minimumDistances(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
