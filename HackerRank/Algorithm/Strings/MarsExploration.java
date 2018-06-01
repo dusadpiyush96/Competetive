dd0504132c03e9b3f31c7f55fd050d2a5c646960
Piyush Dusad <dusadpiyush96@gmail.com>
2018-06-01 10:06:44 +0530

    add MarsExploration.java

---
+++ HackerRank/Algorithm/Strings/MarsExploration.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the marsExploration function below.
+    static int marsExploration(String s) {
+        int count = 0;
+        char[] c = s.toCharArray();
+        for(int i=0;i<c.length-2;i+=3) {
+            if(c[i]!='S') count++;
+            if(c[i+1]!='O') count++;
+            if(c[i+2]!='S') count++;
+        }
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        int result = marsExploration(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
