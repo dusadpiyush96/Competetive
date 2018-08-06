3ccc746648e31fab40adf00c5e7edfb415d18722
Piyush Dusad <dusadpiyush96@gmail.com>
2018-08-06 21:53:28 +0530

    add RunningTime.java

---
+++ HackerRank/Algorithm/Sorting/RunningTime.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the runningTime function below.
+    static int runningTime(int[] arr) {
+        int key = 0;
+        int count =0;
+        for(int i=1;i<arr.length;i++){
+            key = arr[i];
+            for(int j=i-1;j>=0;j--){
+                if(arr[j]>key){
+                    //int k = arr[j];
+                    arr[j+1]=arr[j];
+                    arr[j]=key;
+                    count++;
+                    continue;
+                }
+                else{
+                   // arr[j]=key;
+                    break;
+                }
+            }
+            //print(arr);
+        }
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = runningTime(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
